02b090aa6a1732beb56b7a84edeb439a633e809a
轶哥 <a@wyr.me>
2018-09-17 23:23:51 +0800

    update

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
 <?php
-
-// Kickstart the framework
 $f3=require('lib/base.php');
 
 $f3->set('DEBUG',1);
@@ -15,78 +13,5 @@ $f3->route('GET /',
         echo View::instance()->render('home.html');
     }
 );
-
-// $f3->route('GET /tz',
-// 	function($f3) {
-// 		$classes=array(
-// 			'Base'=>
-// 				array(
-// 					'hash',
-// 					'json',
-// 					'session',
-// 					'mbstring'
-// 				),
-// 			'Cache'=>
-// 				array(
-// 					'apc',
-// 					'apcu',
-// 					'memcache',
-// 					'memcached',
-// 					'redis',
-// 					'wincache',
-// 					'xcache'
-// 				),
-// 			'DB\SQL'=>
-// 				array(
-// 					'pdo',
-// 					'pdo_dblib',
-// 					'pdo_mssql',
-// 					'pdo_mysql',
-// 					'pdo_odbc',
-// 					'pdo_pgsql',
-// 					'pdo_sqlite',
-// 					'pdo_sqlsrv'
-// 				),
-// 			'DB\Jig'=>
-// 				array('json'),
-// 			'DB\Mongo'=>
-// 				array(
-// 					'json',
-// 					'mongo'
-// 				),
-// 			'Auth'=>
-// 				array('ldap','pdo'),
-// 			'Bcrypt'=>
-// 				array(
-// 					'mcrypt',
-// 					'openssl'
-// 				),
-// 			'Image'=>
-// 				array('gd'),
-// 			'Lexicon'=>
-// 				array('iconv'),
-// 			'SMTP'=>
-// 				array('openssl'),
-// 			'Web'=>
-// 				array('curl','openssl','simplexml'),
-// 			'Web\Geo'=>
-// 				array('geoip','json'),
-// 			'Web\OpenID'=>
-// 				array('json','simplexml'),
-// 			'Web\Pingback'=>
-// 				array('dom','xmlrpc')
-// 		);
-// 		$f3->set('classes',$classes);
-// 		$f3->set('content','welcome.htm');
-// 		echo View::instance()->render('layout.htm');
-// 	}
-// );
-
-// $f3->route('GET /userref',
-// 	function($f3) {
-// 		$f3->set('content','userref.htm');
-// 		echo View::instance()->render('layout.htm');
-// 	}
-// );
 
 $f3->run();
